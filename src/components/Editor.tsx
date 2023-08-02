f6a29c990abc0f917d7c4f14944689f590c2fb40
Everson Soares <eversonv4@gmail.com>
2023-08-02 11:46:31 -0300

    feat: uploading to github

---
+++ src/components/Editor.tsx
@@ -0,0 +1,154 @@
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
+} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import js from "highlight.js/lib/languages/javascript";
+import css from "highlight.js/lib/languages/css";
+import ts from "highlight.js/lib/languages/typescript";
+import html from "highlight.js/lib/languages/xml";
+import "highlight.js/styles/atom-one-dark.css";
+import { lowlight } from "lowlight/lib/core";
+import {
+  RxFontBold,
+  RxFontItalic,
+  RxStrikethrough,
+  RxCode,
+  RxChevronDown,
+  RxChatBubble,
+} from "react-icons/rx";
+
+import { InitialContent } from "./initialContent";
+import { BubbleButton } from "./BubbleButton";
+
+lowlight.registerLanguage("html", html);
+lowlight.registerLanguage("css", css);
+lowlight.registerLanguage("js", js);
+lowlight.registerLanguage("ts", ts);
+
+export function Editor() {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
+    ],
+    onUpdate(editor) {
+      // editor.editor.getHTML()
+    },
+    content: InitialContent,
+    editorProps: {
+      attributes: {
+        class: "outline-none",
+      },
+    },
+  });
+
+  return (
+    <>
+      <EditorContent
+        editor={editor}
+        className="max-w-[700px] max-auto pt-16 prose prose-invert prose-emerald"
+      />
+
+      {/* Isso aqui é um menu que abre quando iniciamos uma nova linha e apertamos "/", tipo no notion */}
+      {editor && (
+        <FloatingMenu
+          className="bg-zinc-700 py-2 px-1 shadow-xl border gap-1 border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex flex-col"
+          editor={editor}
+          shouldShow={({ state }) => {
+            const { $from } = state.selection;
+            const currentLineText = $from.nodeBefore?.textContent;
+
+            return currentLineText === "/";
+          }}
+        >
+          <button className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600">
+            <img
+              src="http://www.notion.so/images/blocks/text/en-US.png"
+              alt="text"
+              className="w-12 border border-zinc-600 rounded"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Text</span>
+              <span className="text-xm text-zinc-400">
+                Just start writing with plain text.
+              </span>
+            </div>
+          </button>
+
+          <button
+            className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+          >
+            <img
+              src="http://www.notion.so/images/blocks/header.57a7576a.png"
+              alt="Heading"
+              className="w-12 border border-zinc-600 rounded"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Heading 1</span>
+              <span className="text-xm text-zinc-400">
+                Big section heading.
+              </span>
+            </div>
+          </button>
+        </FloatingMenu>
+      )}
+
+      {/* Isso aqui abaixo são os botõeszinhos que aparecem quando selecionamos um texto */}
+      {editor && (
+        <BubbleMenu
+          editor={editor}
+          className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600"
+        >
+          <BubbleButton>
+            Text
+            <RxChevronDown className="h-4 w-4" />
+          </BubbleButton>
+
+          <BubbleButton>
+            Comment
+            <RxChatBubble className="h-4 w-4" />
+          </BubbleButton>
+
+          <div className="flex items-center">
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              data-active={editor.isActive("bold")}
+            >
+              <RxFontBold className="h-4 w-4" />
+            </BubbleButton>
+
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              data-active={editor.isActive("italic")}
+            >
+              <RxFontItalic className="h-4 w-4" />
+            </BubbleButton>
+
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              data-active={editor.isActive("strike")}
+            >
+              <RxStrikethrough className="h-4 w-4" />
+            </BubbleButton>
+
+            <BubbleButton
+              onClick={() => editor.chain().focus().toggleCode().run()}
+              data-active={editor.isActive("code")}
+            >
+              <RxCode className="h-4 w-4" />
+            </BubbleButton>
+          </div>
+        </BubbleMenu>
+      )}
+    </>
+  );
+}
---
+++ src/components/initialContent.ts
@@ -0,0 +1,119 @@
+export const InitialContent = `
+<header>
+<h1>The Power of Tailwind CSS: Streamline Your Web Development</h1>
+<p>
+  Published on <time dateTime="2023-08-02">August 2, 2023</time>
+</p>
+</header>
+
+<article>
+<p>
+  If you're a web developer, you're probably always on the lookout for
+  tools and frameworks that can help you work more efficiently and
+  produce high-quality results. One such tool that has gained
+  significant popularity in recent years is <strong>Tailwind CSS</strong>.
+</p>
+
+<p>
+  Tailwind CSS is a utility-first CSS framework that provides a set of
+  pre-designed classes to help you quickly build responsive and
+  attractive user interfaces. Instead of writing custom CSS for every
+  styling element, you can simply apply these classes directly in your
+  HTML markup.
+</p>
+
+<p>
+  You can learn more about Tailwindcss at
+  <a href="https://tailwindcss.com/docs/installation/using-postcss">
+    tailwindcss.com
+  </a>
+</p>
+
+<h2>Advantages of Tailwind CSS</h2>
+<p>
+  Let's take a look at some of the key advantages of using Tailwind CSS:
+</p>
+
+<ol>
+  <li>
+    <strong>Rapid Development:</strong> Tailwind CSS allows you to build
+    complex layouts and designs with minimal effort. You can achieve a
+    consistent look and feel throughout your project by reusing utility
+    classes.
+  </li>
+  <li>
+    <strong>Customization:</strong> While Tailwind provides a default
+    design system, you can easily customize the framework to match your
+    project's branding. Define your own color palette, typography,
+    spacing, and more.
+  </li>
+  <li>
+    <strong>Responsive Design:</strong> Creating responsive designs is a
+    breeze with Tailwind's responsive utility classes. You can control
+    the appearance of elements on different screen sizes without writing
+    custom media queries.
+  </li>
+</ol>
+
+<p>
+  Here's an example of how you can use Tailwind CSS classes to create a
+  simple card component:
+</p>
+
+<pre>
+  <code className="language-html">
+    &lt;div class="bg-white p-4 rounded-lg shadow-md"&gt; 
+      &lt;h3 class="text-xl font-semibold"&gt;Welcome to our website!&lt;/h3&gt;
+      &lt;p class="text-gray-600 mt-2"&gt;
+        Explore our services and discover amazing offers.
+      &lt;/p&gt;
+    &lt;/div&gt;
+  </code>
+</pre>
+
+<blockquote>
+  <p>
+    "Tailwind CSS has revolutionized the way I approach web development.
+    It saves me time and allows me to focus on crafting great user
+    experiences."
+  </p>
+  <cite>John Doe, Frontend Developer</cite>
+</blockquote>
+
+<h2>Getting Started</h2>
+<p>
+  Getting started with Tailwind CSS is straightforward. You can install
+  it using a package manager like npm or yarn:
+</p>
+
+<pre>
+  <code className="language-shell">
+    # Using npm npm install tailwindcss # Using yarn yarn add
+    tailwindcss
+  </code>
+</pre>
+
+<p>
+  Once installed, you'll need to set up your CSS file to include
+  Tailwind's styles. You can then start adding Tailwind classes to your
+  HTML elements and witness the magic in action!
+</p>
+
+<h2>Conclusion</h2>
+<p>
+  Tailwind CSS is a game-changer for modern web development. Its
+  utility-first approach, customization options, and responsive design
+  capabilities make it a top choice for developers looking to streamline
+  their workflow and build stunning web interfaces.
+</p>
+
+<p>
+  Give Tailwind CSS a try on your next project, and experience the joy
+  of efficient and effective styling!
+</p>
+</article>
+
+<footer>
+<p>&copy; 2023 YourWebsite.com. All rights reserved.</p>
+</footer>
+`;
